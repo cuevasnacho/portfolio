b8a75b526b9a4ebebe6e00713db11404c8ee4463
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-12-01 16:01:25 -0300

    Footer half done
    Should improve design and add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,20 @@ import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <div className={styles.footer}>
-      <ul className={styles.footer__links}>
-        <li>Home</li>
-        <li>About me</li>
-        <li>Contact</li>
-        <li>Repo</li>
-      </ul>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.footer__column}>
+        <h2>Jump to</h2>
+        <a>Home</a>
+        <a>Projects</a>
+        <a>Skills</a>
+        <a>Courses</a>
+      </div>
+      <div className={styles.footer__column}>
+        <h2>Contact me</h2>
+        <a>Email: icuevas.icc@gmail.com</a>
+        <a>Phone: +54 3571 522614</a>
+      </div>
+    </footer>
   );
 }
 
