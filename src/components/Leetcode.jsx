402ff8e47bfad838ad0d53e878540b72f0350714
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-12-01 16:01:53 -0300

    Graphs added

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -1,8 +1,28 @@
 import { useState, useEffect } from "react";
+import Charts from "react-apexcharts";
 import styles from './Leetcode.module.css';
 
 function Leetcode() {
   const [data, setData] = useState(null);
+  const [series, setSeries] = useState([]);
+  const options = {
+    chart: {
+      type: 'donut',
+    },
+    labels: ['Easy', 'Medium', 'Hard'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 300
+        },
+        legend: {
+          position: 'left'
+        }
+      }
+    }]
+  };
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,17 +41,26 @@ function Leetcode() {
     fetchData();
   },[]);
 
+  useEffect(() => {
+    if (data != null)
+      setSeries([data.easySolved, data.mediumSolved, data.hardSolved]);
+  },[data]);
+
   return (
     <div className={styles.leetsection}>
       {data && (
-        <div>
+        <div className={styles.leetsection__data}>
           <h2>Leetcode Problems</h2>
-          <p>totalSolved: {data.totalSolved}</p>
-          <p>easySolved: {data.easySolved}</p>
-          <p>mediumSolved: {data.mediumSolved}</p>
-          <p>hardSolved: {data.hardSolved}</p>
+          <p>Total problems solved: {data.totalSolved}</p>
+          <p>Contribution points: {data.contributionPoints}</p>
         </div>
       )}
+      <Charts
+        options={options}
+        series={series}
+        width={350}
+        type="donut"
+        />
     </div>
   )
 }
